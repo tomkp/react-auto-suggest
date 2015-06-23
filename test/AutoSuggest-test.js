0f7dceddd59c9f6949dbf408507afd54faf60991
tomkp <tom@tomkp.com>
2015-06-23 07:28:18 +0100

    es6 all the way

--- test/AutoSuggest-test.js
+++ test/AutoSuggest-test.js
@@ -12,17 +12,17 @@ const expect = chai.expect;
 describe('AutoSuggest', () => {
 
 
-    const fetchSuggestions = function(value, callback) {
+    const fetchSuggestions = (value, callback) => {
         callback(['one', 'two', 'three']);
     };
 
 
-    const onSuggestion = function(suggestion) {
+    const onSuggestion = (suggestion) => {
         console.info('onSuggestion', suggestion);
     };
 
 
-    it('should render', function() {
+    it('should render', () => {
         const autoSuggest = TestUtils.renderIntoDocument(
             <AutoSuggest suggestions={fetchSuggestions} onSuggestion={onSuggestion}/>
         );
@@ -31,7 +31,7 @@ describe('AutoSuggest', () => {
     });
 
 
-    it('should not display the DropDown', function() {
+    it('should not display the DropDown', () => {
         const autoSuggest = TestUtils.renderIntoDocument(
             <AutoSuggest suggestions={fetchSuggestions} onSuggestion={onSuggestion}/>
         );
@@ -42,7 +42,7 @@ describe('AutoSuggest', () => {
     });
 
 
-    it('should display the DropDown when text is entered', function() {
+    it('should display the DropDown when text is entered', () => {
         const autoSuggest = TestUtils.renderIntoDocument(
             <AutoSuggest suggestions={fetchSuggestions} onSuggestion={onSuggestion}/>
         );
@@ -54,7 +54,7 @@ describe('AutoSuggest', () => {
     });
 
 
-    it('should display the suggestions when the DropDown is displayed', function() {
+    it('should display the suggestions when the DropDown is displayed', () => {
         const autoSuggest = TestUtils.renderIntoDocument(
             <AutoSuggest suggestions={fetchSuggestions} onSuggestion={onSuggestion}/>
         );
@@ -65,7 +65,7 @@ describe('AutoSuggest', () => {
     });
 
 
-    it('should select the next suggestion when the down key is pressed', function() {
+    it('should select the next suggestion when the down key is pressed', () => {
         const autoSuggest = TestUtils.renderIntoDocument(
             <AutoSuggest suggestions={fetchSuggestions} onSuggestion={onSuggestion}/>
         );
@@ -81,7 +81,7 @@ describe('AutoSuggest', () => {
     });
 
 
-    it('should select the previous suggestion when the up key is pressed', function() {
+    it('should select the previous suggestion when the up key is pressed', () => {
         const autoSuggest = TestUtils.renderIntoDocument(
             <AutoSuggest suggestions={fetchSuggestions} onSuggestion={onSuggestion}/>
         );
@@ -97,7 +97,7 @@ describe('AutoSuggest', () => {
     });
 
 
-    it('should display entered text', function() {
+    it('should display entered text', () => {
         const autoSuggest = TestUtils.renderIntoDocument(
             <AutoSuggest suggestions={fetchSuggestions} onSuggestion={onSuggestion}/>
         );
@@ -107,7 +107,7 @@ describe('AutoSuggest', () => {
     });
 
 
-    it('should pass entered text to suggestions function', function(done) {
+    it('should pass entered text to suggestions function', (done) => {
         const fetchSuggestions = function(suggestion) {
             expect(suggestion).to.equal('cat');
             done();
@@ -122,7 +122,7 @@ describe('AutoSuggest', () => {
     });
 
 
-    it('should hide the dropdown when the esc key is pressed', function() {
+    it('should hide the dropdown when the esc key is pressed', () => {
         const autoSuggest = TestUtils.renderIntoDocument(
             <AutoSuggest suggestions={fetchSuggestions} onSuggestion={onSuggestion}/>
         );
@@ -133,7 +133,7 @@ describe('AutoSuggest', () => {
     });
 
 
-    it('should allow custom dropdowns', function() {
+    it('should allow custom dropdowns', () => {
         const CustomSuggestionRenderer = React.createClass({
             render() {
                 return <div className="Suggestion">{this.props.suggestion}</div>;
@@ -152,7 +152,7 @@ describe('AutoSuggest', () => {
     });
 
 
-    it('should allow custom dropdowns for arrays of json', function() {
+    it('should allow custom dropdowns for arrays of json', () => {
         const fetchSuggestions = function(value, callback) {
             callback([{title:'one'}, {title:'two'}, {title:'three'}]);
         };
@@ -176,7 +176,7 @@ describe('AutoSuggest', () => {
 
 
 
-    it('should handle displaying term from custom dropdowns', function() {
+    it('should handle displaying term from custom dropdowns', () => {
         const fetchSuggestions = function(value, callback) {
             callback([{title:'cow'}, {title:'cat'}, {title:'chicken'}]);
         };
